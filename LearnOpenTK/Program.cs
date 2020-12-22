85cdbec38fbe733b9275ff598ea372de8efa7b54
秋雨落 <i@rain.cx>
2020-12-22 10:01:30 +0800

    :tada: Init
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ LearnOpenTK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnOpenTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
