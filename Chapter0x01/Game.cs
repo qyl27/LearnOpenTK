a07ed67141f0d1cec090005856f5f4ea08de3ee9
秋雨落 <i@rain.cx>
2022-09-18 08:08:10 +0800

    feat: Chapter 0x01.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ Chapter0x01/Game.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Chapter0x01;
+
+public class Game : GameWindow
+{
+    public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) 
+        : base(gameWindowSettings, nativeWindowSettings)
+    {
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        if (KeyboardState.IsKeyPressed(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}
---
+++ Chapter0x01/Program.cs
@@ -0,0 +1,14 @@
+﻿using Chapter0x01;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+using var game = new Game(
+    new GameWindowSettings(),
+    new NativeWindowSettings
+    {
+        Size = new Vector2i(640, 480), 
+        Title = "0x01 Hello, Window!"
+    }
+);
+
+game.Run();
